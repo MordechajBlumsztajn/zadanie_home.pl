1d385df4d352146a9d86b2a21e1291489c42297a
Piotr Wasilewski <ptr.wasilewski@outlook.com>
2021-05-22 15:57:04 +0200

    refactored and improved code responsible for input processing

--- main.c
+++ main.c
@@ -1,102 +1,231 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
+#include <errno.h>
 
-const int MIN_ARGUMENT_COUNT = 2;
-const int MAX_EXCLUSIVE_OPTIONS = 1;
+enum OPTS_FLAG
+{
+    NO_OPT_FLAG     = 0x0,
 
-enum OPT_FLAG { 
-    FLAG_a = 1, 
-    FLAG_u = 2, 
-    FLAG_n = 4, 
-    FLAG_f = 8 
-    };
+    OPT_a_FLAG      = 0x1, // mandatory and exclusive
+    OPT_u_FLAG      = 0x2, // mandatory and exclusive
+    OPT_n_FLAG      = 0x4, // mandatory and exclusive
 
-struct dupa {
-
+    OPT_f_FLAG      = 0x8 // optional
 };
 
-int main(int argc, char *argv[])
+enum OPTS_WITH_ARGS_INDEX_ORDER
 {
-    bool failure_occurred = false;
+    OPT_u_INDEX = 0,
+    OPT_n_INDEX = 1,
+    OPT_f_INDEX = 2
+};
 
-    bool chosen_opt_a = false;
-    bool chosen_option_u = false;
-    bool chosen_option_n = false;
+enum OPTS_ERROR_FLAG
+{
+    NO_ERROR                = 0x0,
+    UNKNOWN_OPTION_ERROR    = 0x1,
+    ARG_REQUIRED_ERROR      = 0x2,
+    MANDATORY_OPT_ERROR     = 0x4,
+    EXCLUSIVE_OPTS_ERROR    = 0x8,
+    NON_OPT_ARG_ERROR       = 0x10
+};
 
-    int exclusive_option_counter = 0;
+enum MAIN_FUNC_RESULT 
+{
+    SUCCESS_RESULT = 0,
+    FAILURE_RESULT = 1
+};
+
+struct flags_container
+{
+    int opt_flags;
+    int errors_flags;
+};
+
+typedef struct flags_container FC;
+
+FC generate_empty_flags_container()
+{
+    FC flags;
+    flags.opt_flags = NO_OPT_FLAG;
+    flags.errors_flags = NO_ERROR;
+}
+
+void copy_option_argument(char** destination, enum OPTS_WITH_ARGS_INDEX_ORDER index, char* source)
+{
+    destination[index] = (char*) malloc((strlen(source) + 1) * sizeof(char));
+    if (NULL == destination[index])
+    {
+        exit(ENOMEM);
+    }
+    strcpy(destination[index], source);
+}
+
+FC parse_input(int argc, char* argv[], char* optsargs[], FC flags)
+{
+    const char* const OPTS_STRING = ":au:n:f:";
 
     int opt = 0;
-    while ((opt = getopt(argc, argv, ":au:n:f:")) != -1)
+    while ((opt = getopt(argc, argv, OPTS_STRING)) != -1)
     {
         switch (opt)
         {
         case 'a':
-            exclusive_option_counter++;
-            chosen_opt_a = true;
+            flags.opt_flags |= OPT_a_FLAG;
             break;
         case 'u':
-            exclusive_option_counter++;
-            chosen_option_u = true;
+            copy_option_argument(optsargs, OPT_u_INDEX, optarg);
+            flags.opt_flags |= OPT_u_FLAG;
             break;
         case 'n':
-            exclusive_option_counter++;
-            chosen_option_n = true;
+            copy_option_argument(optsargs, OPT_n_INDEX, optarg);
+            flags.opt_flags |= OPT_n_FLAG;
             break;
         case 'f':
+            copy_option_argument(optsargs, OPT_f_INDEX, optarg);
+            flags.opt_flags |= OPT_f_FLAG;
             break;
         case '?':
+            flags.errors_flags |= UNKNOWN_OPTION_ERROR;
             fprintf(stderr, "error: Unknown option: %c.\n", optopt);
-            failure_occurred = true;
             break;
         case ':':
+            flags.errors_flags |= ARG_REQUIRED_ERROR;
             fprintf(stderr, "error: Option %c requires an argument.\n", optopt);
-            failure_occurred = true;
+
+            switch (optopt)
+            {
+            case 'u':
+                flags.opt_flags |= OPT_u_FLAG;
+                break;
+            case 'n':
+                flags.opt_flags |= OPT_n_FLAG;
+                break;
+            }
+            
             break;
         }
     }
+    return flags;
+}
 
-    if (0 == exclusive_option_counter)
+FC check_for_mandatory_option(FC flags)
+{
+    if (!(flags.opt_flags & (OPT_a_FLAG | OPT_u_FLAG | OPT_n_FLAG)))
     {
-        fprintf(stderr, "error: One (and only one) of these options needs to be chosen: -a, -u, -n.\n");
-        failure_occurred = true;
+        fprintf(stderr, "error: One of these options needs to be chosen: -a, -u, -n.\n");
+        flags.errors_flags |= MANDATORY_OPT_ERROR;
     }
-    else if (MAX_EXCLUSIVE_OPTIONS < exclusive_option_counter)
+    return flags;
+}
+
+FC check_for_options_exclusivity(FC flags)
+{
+    const int MAX_EXCLUSIVE_OPTS_COUNT = 1;
+
+    if ((((flags.opt_flags & OPT_a_FLAG) > 0) + ((flags.opt_flags & OPT_u_FLAG) > 0) + 
+            ((flags.opt_flags & OPT_n_FLAG) > 0)) > MAX_EXCLUSIVE_OPTS_COUNT)
     {
         fprintf(stderr, "error: Only one of these options can be chosen: -a, -u, -n.\n");
-        failure_occurred = true;
+        flags.errors_flags |= EXCLUSIVE_OPTS_ERROR;
     }
+    return flags;
+}
 
+FC check_for_non_optional_arguments(int argc, char* argv[], FC flags)
+{
     if (optind < argc)
     {
-        fprintf(stderr, "error: Program does not accept non-optional arguments: ");
-        failure_occurred = true;
-
+        fprintf(stderr, "error: Program does not accept non-optional arguments:");
+    
         while (optind < argc)
         {
-            printf(" %s", argv[optind++]);
+            printf(" \"%s\"", argv[optind++]);
         }
+        printf(".\n");
+        flags.errors_flags |= NON_OPT_ARG_ERROR;
+    }
+
+    return flags;
+}
+
+bool error_occurred(FC flags)
+{
+    return flags.errors_flags > 0;
+}
+
+char** allocate_memory_for_option_arguments(int args_count)
+{
+    char** optargs = (char**) malloc(args_count * sizeof(char*));
+    if (NULL == optargs)
+    {
+        exit(ENOMEM);
+    }
+
+    for (size_t i = 0; i < args_count; i++)
+    {
+        optargs[i] = NULL;
+    }
+
+    return optargs;
+}
+
+void deallocate_option_arguments_memory(char** optargs, int args_count)
+{
+    for (size_t i = 0; i < args_count; i++)
+    {
+        free(optargs[i]);
+    }
+    
+    free(optargs);
+}
+
+void execute_chosen_command(FC flags, char* opts_args[])
+{
+    fprintf(stdout, "ARGUMENTS: ");
+        if (flags.opt_flags & OPT_u_FLAG)
+        {
+            printf("-u %s ", opts_args[OPT_u_INDEX]);
+        }
+
+        if (flags.opt_flags & OPT_n_FLAG)
+        {
+            printf("-n %s ", opts_args[OPT_n_INDEX]);
+        }
+
+        if (flags.opt_flags & OPT_f_FLAG)
+        {
+            printf("-f %s ", opts_args[OPT_f_INDEX]);
+        }
+
         printf("\n");
-    }
+}
 
-    if (true == failure_occurred)
+int main(int argc, char *argv[])
+{
+    int result = SUCCESS_RESULT;
+
+    const int OPTS_WITH_ARGS_COUNT = 3;
+    char** optargs = allocate_memory_for_option_arguments(OPTS_WITH_ARGS_COUNT);
+
+    FC flags = generate_empty_flags_container();
+    flags = parse_input(argc, argv, optargs, flags);
+    flags = check_for_mandatory_option(flags);
+    flags = check_for_options_exclusivity(flags);
+    flags = check_for_non_optional_arguments(argc, argv, flags);
+
+    if (error_occurred(flags))
     {
-        fprintf(stderr, "Terminating execution...\n");
-        return 1;
+        result = FAILURE_RESULT;
     }
-
-    if (true == chosen_opt_a)
+    else
     {
-
+        execute_chosen_command(flags, optargs);
     }
-    else if (true == chosen_option_n)
-    {
+    deallocate_option_arguments_memory(optargs, OPTS_WITH_ARGS_COUNT);
 
-    }
-    else if (true == chosen_option_u)
-    {
-        
-    }
-
-    return 0;
+    return result;
 }
