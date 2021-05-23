3554439d29192855003724391456b961b637983d
Piotr Wasilewski <ptr.wasilewski@outlook.com>
2021-05-24 00:25:34 +0200

    refactored print_all_procs_info (-a) and changed source of proc name to /proc/<pid>/comm

--- process_info/src/process_info.c
+++ process_info/src/process_info.c
@@ -1,9 +1,18 @@
 #include "process_info.h"
 
 const char *const PROC_PATH = "/proc";
+
+/* TO DELETE ? */
 const char *const FORWARD_SLASH_STR = "/";
+
+/* TO DELETE */
 const char *const CMDLINE_FILENAME = "cmdline";
+
+const char *const PROC_NAME_FILENAME = "comm";
+
+/* TO DELETE */
 const char *const ZOMBIE_PROCESS_STR = "[Z]";
+
 const char FORWARD_SLASH_CHAR = '/';
 
 FILE *open_file_to_write_if_requested(char *filename)
@@ -51,7 +60,7 @@ bool is_string_a_number(char *str)
     return result;
 }
 
-char* get_last_element_from_path(char* path)
+char *get_last_element_from_path(char *path)
 {
     for (int i = strlen(path) - 1; i >= 0; i--)
     {
@@ -61,117 +70,116 @@ char* get_last_element_from_path(char* path)
             break;
         }
     }
-    
+
     return path;
+}
+
+char *create_path_from_2_strings(const char *const left, const char *const right)
+{
+    static const char *const FORWARD_SLASH_STR = "/";
+
+    char *path = (char *)malloc(
+        (strlen(left) +
+         strlen(FORWARD_SLASH_STR) +
+         strlen(right) +
+         1) *
+        sizeof(char));
+
+    if (NULL == path)
+    {
+        fprintf(stderr, "critical error: %s\n", strerror(errno));
+        exit(errno);
+    }
+
+    strcpy(path, left);
+    strcat(path, FORWARD_SLASH_STR);
+    strcat(path, right);
+
+    return path;
+}
+
+DIR *opendir_and_verify(const char *const dirpath)
+{
+    DIR *ptr_proc_dir = opendir(dirpath);
+    if (NULL == ptr_proc_dir)
+    {
+        fprintf(stderr, "critical error: %s\n", strerror(errno));
+        exit(errno);
+    }
+    return ptr_proc_dir;
+}
+
+unsigned get_file_content_length(const char *const path)
+{
+    static const char *const read_mode = "r";
+    int read_char = '\0';
+    unsigned str_length = 0;
+    
+    FILE *stream = fopen(path, read_mode);
+    if (NULL == stream)
+    {
+        fprintf(stderr, "critical error: %s\n", strerror(errno));
+        exit(errno);
+    }
+
+    while ('\n' != (read_char = fgetc(stream)) && !feof(stream))
+    {
+        str_length++;
+    }
+
+    fclose(stream);
+    return str_length;
+}
+
+char *get_file_content(const char *const path, unsigned length)
+{
+    static const char *const read_mode = "r";
+    char *file_content = (char *)calloc((length + 1), sizeof(char));
+    int read_char = '\0';
+
+    FILE *stream = fopen(path, read_mode);
+    if (NULL == stream)
+    {
+        fprintf(stderr, "critical error: %s\n", strerror(errno));
+        exit(errno);
+    }
+
+    int i = 0;
+    while ('\n' != (read_char = fgetc(stream)) && !feof(stream))
+    {
+        file_content[i] = read_char;
+        i++;
+    }
+
+    return file_content;
 }
 
 // option -a
 void print_all_procs_info(char *filename)
 {
     FILE *stream = open_file_to_write_if_requested(filename);
-
     struct dirent *ptr_proc_dirent = NULL;
-    DIR *ptr_proc_dir = opendir(PROC_PATH);
-    if (NULL == ptr_proc_dir)
-    {
-        close_file_if_not_stdout(stream);
-        fprintf(stderr, "critical error: %s\n", strerror(errno));
-        exit(errno);
-    }
+    DIR *ptr_proc_dir = opendir_and_verify(PROC_PATH);
 
     while ((ptr_proc_dirent = readdir(ptr_proc_dir)) != NULL)
     {
         if (is_string_a_number(ptr_proc_dirent->d_name))
         {
-            char *proc_subpath = (char *)malloc(
-                (strlen(PROC_PATH) +
-                 strlen(FORWARD_SLASH_STR) +
-                 strlen(ptr_proc_dirent->d_name) +
-                 1) *
-                sizeof(char));
-
-            strcpy(proc_subpath, PROC_PATH);
-            strcat(proc_subpath, FORWARD_SLASH_STR);
-            strcat(proc_subpath, ptr_proc_dirent->d_name);
-
+            char *proc_subpath = create_path_from_2_strings(PROC_PATH, ptr_proc_dirent->d_name);
             struct dirent *ptr_proc_subdirent = NULL;
-            DIR *ptr_proc_subdir = opendir(proc_subpath);
-            if (NULL == ptr_proc_subdir)
-            {
-                close_file_if_not_stdout(stream);
-                fprintf(stderr, "critical error: %s\n", strerror(errno));
-                exit(errno);
-            }
+            DIR *ptr_proc_subdir = opendir_and_verify(proc_subpath);
 
             while ((ptr_proc_subdirent = readdir(ptr_proc_subdir)) != NULL)
             {
-                if (!strcmp(ptr_proc_subdirent->d_name, CMDLINE_FILENAME))
+                if (!strcmp(ptr_proc_subdirent->d_name, PROC_NAME_FILENAME))
                 {
-                    /* READING CMDLINE */
-                    char *cmdline_path = (char *)malloc(
-                        (strlen(proc_subpath) +
-                         strlen(FORWARD_SLASH_STR) +
-                         strlen(CMDLINE_FILENAME) +
-                         1) *
-                        sizeof(char));
+                    char *comm_path = create_path_from_2_strings(proc_subpath, PROC_NAME_FILENAME);
+                    unsigned str_length = get_file_content_length(comm_path);
+                    char *comm_content = get_file_content(comm_path, str_length);
+                    fprintf(stream, "%s %s\n", comm_content, ptr_proc_dirent->d_name);
 
-                    strcpy(cmdline_path, proc_subpath);
-                    strcat(cmdline_path, FORWARD_SLASH_STR);
-                    strcat(cmdline_path, CMDLINE_FILENAME);
-
-                    /* GETTING STRING LENGTH */
-                    FILE *cmdline_stream = NULL;
-                    int read_char = 'a';
-                    int str_length = 0;
-                    static const char *const mode = "r";
-
-                    cmdline_stream = fopen(cmdline_path, mode);
-                    if (NULL == cmdline_stream)
-                    {
-                        fprintf(stderr, "critical error: %s\n", strerror(errno));
-                        exit(errno);
-                    }
-
-                    while ('\0' != (read_char = fgetc(cmdline_stream)) && ' ' != read_char && !feof(cmdline_stream))
-                    {
-                        str_length++;
-                    }
-
-                    fclose(cmdline_stream);
-
-                    /* GETTING STRING */
-                    if (str_length)
-                    {
-                        char *cmdline_content = (char *)malloc((str_length + 1) * sizeof(char));
-                        
-                        cmdline_stream = fopen(cmdline_path, mode);
-                        if (NULL == cmdline_stream)
-                        {
-                            fprintf(stderr, "critical error: %s\n", strerror(errno));
-                            exit(errno);
-                        }
-
-                        read_char = 'a';
-                        int i = 0;
-                        while ('\0' != (read_char = fgetc(cmdline_stream)) && ' ' != read_char && !feof(cmdline_stream))
-                        {
-                            cmdline_content[i] = read_char;
-                            i++;
-                        }
-                        cmdline_content[i] = '\0';
-
-                        /* PARSE STRING */
-                        cmdline_content = get_last_element_from_path(cmdline_content);
-                        fprintf(stream, "%s %s\n", cmdline_content, ptr_proc_dirent->d_name);
-
-                        free(cmdline_content);
-                    }
-                    else 
-                    {
-                        fprintf(stream, "%s %s\n", ZOMBIE_PROCESS_STR, ptr_proc_dirent->d_name);
-                    }
-
-                    free(cmdline_path);
+                    free(comm_content);
+                    free(comm_path);
                     break;
                 }
             }
